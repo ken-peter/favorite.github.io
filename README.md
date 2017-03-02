70c5259840b01a119db65f7e03b57fcbf7a7cbe8
ken-peter <Ken Peter>
2017-03-02 07:50:48 +0300

    added a README file

---
+++ README.md
@@ -0,0 +1,24 @@
+# Project Name
+
+MY FAVORITE THINGS
+
+##Author
+Kennedy Peter
+
+## Description
+
+The project is simply used to display the user's input into list form.
+
+### Technologies Used
+* HTML
+* CSS
+* JavaScript
+
+## Setup
+
+Make sure you have git installed. On the terminal, navigate to your workspace directory and run
+
+
+## Contributing
+
+I welcome pull requests
--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
-    $("#blanks form").submit(function(event) {
-      var favs = ["food", "sport", "quote", "pet"];
+  $("#blanks form").submit(function(event) {
+    var favs = ["food", "sport", "quote", "pet"];
 
-      favs.forEach(function(fav) {
-        var userInput = $("input#" + fav).val();
-        $("#mylist").append("<li>" + userInput + "</li>");
-      });
+    favs.forEach(function(fav) {
+      var userInput = $("input#" + fav).val();
+      $("#mylist").append("<li>" + userInput + "</li>");
+    });
 
-      $(".list").show();
-
-      event.preventDefault();
-});
+    $(".list").show();
+    var blanks = $("#blanks form")
+    blanks.hide();
+    event.preventDefault();
+  });
 });
